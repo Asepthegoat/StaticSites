5cecfb16b16d7a22cde71cb73057f28ae375279c
Jorell <167979942+Asepthegoat@users.noreply.github.com>
2026-08-10 21:28:34 +0700

    Update bloxfruit.lua

--- bloxfruit.lua
+++ bloxfruit.lua
@@ -24,10 +24,10 @@ local function init()
     task.wait(0.1)
     game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("SetTeam2","Marines")
 end
-game.Loaded:Wait()
-task.wait(4)
-init()
-
+game.Loaded:Connect(function() 
+    task.wait(5) --in case you have low fps
+    init()
+end)
 local function hopserver()
     local Event = game:GetService("ReplicatedStorage").__ServerBrowser
     local tbl = Http:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100&cursor="))
